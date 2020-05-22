cf990171fb4668297a7ab671e93bd294c097f4dc
gw1303 <gw1303@nate.com>
2020-05-22 15:43:11 +0900

    getsongId

--- watermelon_project/watermelon_app/load_data.py
+++ watermelon_project/watermelon_app/load_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-genreDf = pd.read_json('home/gw1303/watermelon/data/genre_gn_all.json', encoding='utf-8')
+genreDf = pd.read_json('home/gw1303/watermelon/data/genre_gn_all.json', typ='series', encoding='utf-8')
 
 
 def test() :
