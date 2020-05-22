129b3419139b6e38ebdf588b3a7bd3b11baeb01a
gw1303 <gw1303@nate.com>
2020-05-22 15:45:30 +0900

    getsongId

--- watermelon_project/watermelon_app/load_data.py
+++ watermelon_project/watermelon_app/load_data.py
@@ -1,8 +1,10 @@
 import pandas as pd
-
+from gensim.models import Word2Vec
 
 genreDf = pd.read_json('home/gw1303/watermelon/data/genre_gn_all.json', typ='series', encoding='utf-8')
 
+# 모델 불러오기
+model = Word2Vec.load('home/gw1303/watermelon/song2vec/song2vec.model')
 
 def test() :
-	return genreDf.iloc[0][0]
+	return genreDf.iloc[10][0]
