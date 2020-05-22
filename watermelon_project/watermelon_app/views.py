b2ba0240eea8f53370141ebe608ce1450a274cee
gw1303 <gw1303@nate.com>
2020-05-22 15:01:31 +0900

    getsongId

--- watermelon_project/watermelon_app/views.py
+++ watermelon_project/watermelon_app/views.py
@@ -12,20 +12,20 @@ import pandas as pd
 from watermelon_app.models import (songDf, playlistDf)
 
 
+def __init__(request) :
+    # song meta data
+    songDf = pd.read_json('home/gw1303/watermelon/data/song_meta.json', encoding='utf-8')
 
-# song meta data
-songDf = pd.read_json('home/gw1303/watermelon/data/song_meta.json', encoding='utf-8')
+    # 대문자 -> 소문자
+    for i in ['album_name', 'song_name', 'artist_name_basket'] :
+        songDf[i] = songDf[i].map(str).map(str.lower)
 
-# 대문자 -> 소문자
-for i in ['album_name', 'song_name', 'artist_name_basket'] :
-    songDf[i] = songDf[i].map(str).map(str.lower)
+    # 플레이리스트 df
+    playlistDf = pd.read_json('home/gw1303/watermelon/data/train.json', encoding='utf-8')
+    playlistDf
 
-# 플레이리스트 df
-playlistDf = pd.read_json('home/gw1303/watermelon/data/train.json', encoding='utf-8')
-playlistDf
-
-# 모델 불러오기
-model = Word2Vec.load('home/gw1303/watermelon/song2vec/song2vec.model')
+    # 모델 불러오기
+    model = Word2Vec.load('home/gw1303/watermelon/song2vec/song2vec.model')
 
 
 # Create your views here.
